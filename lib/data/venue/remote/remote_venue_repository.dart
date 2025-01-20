bddf31132b8ffb51b0157cb09c3eec5792fe1c2c
yunyezl <yun72027@gmail.com>
2025-01-21 01:00:19 +0900

    chore: remove hard-coding response

--- lib/data/venue/remote/remote_venue_repository.dart
+++ lib/data/venue/remote/remote_venue_repository.dart
@@ -24,13 +24,9 @@ class RemoteVenueRepository extends VenueRepository {
 
   @override
   Future<Result<List<Venue>, BaseError>> searchVenues(String keyword) async {
-    // final response = await _client.get('/v1/venues/search');
-    // final List<dynamic> json = response.data['list'];
-    // final venues = json.map((json) => Venue.fromJson(json)).toList();
-    return Success([
-      Venue(id: 123, name: "장충체육관", location: "서울시 중구 동호로"),
-      Venue(id: 125, name: "장충체육관", location: "서울시 중구 동호로"),
-    ]);
+    // TODO: 토큰 넣은 후 값 재확인 필요
+    final response = await _client.get('/v1/venues/search');
+    return response.toListResult(fromJson: Venue.fromJson, key: 'list');
   }
 
   @override
